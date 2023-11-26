68aa476156302594ac041ae91fe5f0061bf61864
Aleksandr Alekhin <aleksandera9@gmail.com>
2023-11-27 00:38:01 +0300

    feat: add points log

--- coffee-house/src/script/index.js
+++ coffee-house/src/script/index.js
@@ -1,1 +1,10 @@
 import '../sass/index.sass';
+
+console.log(`
+- Checking validation of pages: +18
+- The layout matches the design: +40
+- CSS Requirements: +10
+- Interactivity: +32
+
+Total: 100
+`);
