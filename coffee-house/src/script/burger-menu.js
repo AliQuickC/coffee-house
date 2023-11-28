53d32c29f17eea4e7408b1afe1978f4a690e22de
Aleksandr Alekhin <aleksandera9@gmail.com>
2023-11-28 03:10:31 +0300

    fix: burger button styles

--- coffee-house/src/script/burger-menu.js
+++ coffee-house/src/script/burger-menu.js
@@ -1,30 +1,30 @@
-// (function () {
-// 	const burgerButton = document.getElementById('burger-button');
-// 	const burgerMenu = document.getElementById('burger-menu');
+(function () {
+	const burgerButton = document.getElementById('burger-button');
+	const burgerMenu = document.getElementById('burger-menu');
 
-// 	burgerMenu.onclick = () => {
-// 		burgerButton.classList.remove('burger-button_active');
-// 		offBurgerMenu();
-// 	};
+	burgerMenu.onclick = () => {
+		burgerButton.classList.remove('burger-button_active');
+		offBurgerMenu();
+	};
 
-// 	burgerButton.onclick = (event) => {
-// 		event.currentTarget.classList.toggle('burger-button_active');
-// 		if (event.currentTarget.classList.contains('burger-button_active')) {
-// 			onBurgerMenu();
-// 		} else {
-// 			offBurgerMenu();
-// 		}
-// 	};
+	burgerButton.onclick = (event) => {
+		event.currentTarget.classList.toggle('burger-button_active');
+		if (event.currentTarget.classList.contains('burger-button_active')) {
+			onBurgerMenu();
+		} else {
+			offBurgerMenu();
+		}
+	};
 
-// 	function offBurgerMenu() {
-// 		burgerMenu.classList.remove('burger-menu_active');
-// 		document.body.classList.remove('scroll-block');
-// 		document.documentElement.classList.remove('scroll-block');
-// 	}
+	function offBurgerMenu() {
+		burgerMenu.classList.remove('burger-menu_active');
+		document.body.classList.remove('scroll-block');
+		document.documentElement.classList.remove('scroll-block');
+	}
 
-// 	function onBurgerMenu() {
-// 		burgerMenu.classList.add('burger-menu_active');
-// 		document.body.classList.add('scroll-block');
-// 		document.documentElement.classList.add('scroll-block');
-// 	}
-// })();
+	function onBurgerMenu() {
+		burgerMenu.classList.add('burger-menu_active');
+		document.body.classList.add('scroll-block');
+		document.documentElement.classList.add('scroll-block');
+	}
+})();
