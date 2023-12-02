60b331675e01217be3278fc41b03baef8f0a1735
Aleksandr Alekhin <aleksandera9@gmail.com>
2023-12-02 23:02:57 +0300

    feat: burger menu

--- coffee-house/src/script/burger-menu.js
+++ coffee-house/src/script/burger-menu.js
@@ -2,12 +2,16 @@
 	const burgerButton = document.getElementById('burger-button');
 	const burgerMenu = document.getElementById('burger-menu');
 
-	burgerMenu.onclick = () => {
-		burgerButton.classList.remove('burger-button_active');
-		offBurgerMenu();
+	burgerMenu.onclick = (event) => {
+		if (event.target.closest('.burger-menu__item-link')) {
+			burgerButton.classList.remove('burger-button_active');
+			offBurgerMenu();
+		}
+		console.log('event.currentTarget: ', event.target);
 	};
 
 	burgerButton.onclick = (event) => {
+		window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
 		event.currentTarget.classList.toggle('burger-button_active');
 		if (event.currentTarget.classList.contains('burger-button_active')) {
 			onBurgerMenu();
